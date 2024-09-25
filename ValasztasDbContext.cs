c1b3cdf0810840bab257d39510be48f4b9b97c05
BalintfyPeter <72075735+BalintfyPeter@users.noreply.github.com>
2024-09-25 11:33:41 +0200

    Add files via upload

---
+++ ValasztasDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Valasztarsok.Models
+{
+    public class ValasztasDbContext : DbContext
+    {
+        public ValasztasDbContext(DbContextOptions<ValasztasDbContext> options)
+            : base (options)
+        {
+
+        }
+    public DbSet<Jelolt> Jeloltek {  get; set; }
+    public DbSet<Part> Partok { get; set; }
+    }
+
+    
+}
